47adeac5df78eac09fa576fc987fefc9e812c4e0
Niyamaka <niyamaka@yuneta.io>
2020-12-09 16:13:22 +0100

    yuneta version 4.2.28, all node's api has changed

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
 #define APP_NAME        ROLE_CONTROLCENTER
 #define APP_DOC         "Control Center of Yuneta Systems"
 
-#define APP_VERSION     "4.2.27"
+#define APP_VERSION     "4.2.28"
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 

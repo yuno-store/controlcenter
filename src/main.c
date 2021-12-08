309cb4c28c20cd7d95c64a728f7061c61c70256d
Niyamaka <niyamaka@yuneta.io>
2021-12-08 07:36:27 +0100

    v5.0.6

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        ROLE_CONTROLCENTER
 #define APP_DOC         "Control Center of Yuneta Systems"
 
-#define APP_VERSION     "5.0.4"
+#define APP_VERSION     __yuneta_version__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 #define APP_DATETIME    __DATE__ " " __TIME__
 
